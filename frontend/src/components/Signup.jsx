f915c88cf411d73f8ab1c097151c9f8eb1bd9a25
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2024-09-04 18:56:22 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
     };
     
     try {
-      const res = await axios.post("http://localhost:4001/user/signup", userInfo);
+      const res = await axios.post("https://pageunlockbackend.onrender.com/user/signup", userInfo);
       console.log(res.data);
       if (res.data) {
         toast.success("Signed in successfully");
